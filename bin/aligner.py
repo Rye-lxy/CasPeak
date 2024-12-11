443a33852b25981849cbfc98fe63cb0f16e29426
Xinyi Liu <emiya_lxy@foxmail.com>
2024-12-11 04:52:11 +0000

    fix some bugs

--- bin/aligner.py
+++ bin/aligner.py
@@ -9,9 +9,9 @@ from fileReader import *
 def aligner(args):
     # args: read, ref, insert, thread
     os.makedirs("lastdb", exist_ok=True)
-    os.makedirs("lastal", exist_ok=True)
     subprocess.check_call(f"lastdb -P{args.thread} -uRY4 lastdb/ref {args.ref}", shell=True)
     subprocess.check_call(f"lastdb -P{args.thread} -uRY4 lastdb/insert {args.insert}", shell=True)
+    os.makedirs("lastal", exist_ok=True)
     with open("lastal/read_to_ref.maf", "w") as f:
         subprocess.check_call(f"last-train -P{args.thread} -Q0 lastdb/ref {args.read} | lastal -P{args.thread} --split -p - lastdb/ref {args.read}", 
                               stdout=f, shell=True)
