c9baaf167a98a331f0cddb1afcdd3768bb61d19c
Xinyi Liu <emiya_lxy@foxmail.com>
2024-12-11 05:08:30 +0000

    add more info to vcf file

--- bin/vcfFormatter.py
+++ bin/vcfFormatter.py
@@ -8,9 +8,14 @@ def vcfHeader():
     return f"""##fileformat=VCFv4.2
 ##fileDate={datetime.now().strftime("%Y%m%d")}
 ##source=Caspeak
-#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO"""
+##INFO=<ID=END,Number=1,Type=Integer,Description="End position of the structural variant">
+##INFO=<ID=SVTYPE,Number=1,Type=String,Description="Type of structural variant">
+##INFO=<ID=SVLEN,Number=1,Type=Integer,Description="Difference in length between REF and ALT alleles">
+##INFO=<ID=matching_classes,Number=.,Type=String,Description="Repeat class names">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSAMPLE"""
 
 def vcfRecord(chrom, refpos, altStart, altEnd, strand, seq, id):
     alt = seq[altStart-1:altEnd] if strand == "+" else reverseComplement(seq[altStart-1:altEnd])
     ref = alt[0]
-    return f"{chrom}\t{refpos}\tcaspeak_{id}\t{ref}\t{alt}\t.\tPASS\t."
+    return f"{chrom}\t{refpos}\tcaspeak_{id}\t{ref}\t{alt}\t.\tPASS\tEND={refpos};SVTYPE=INS;SVLEN={altEnd-altStart};matching_classes=LINE/L1\tGT\t./."
