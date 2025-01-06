cf3a88f83ea0bc0e844c50d325ee8f49965e2fd2
Xinyi Liu <emiya_lxy@foxmail.com>
2025-01-06 08:52:54 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+commitInfo = "$Format:%d$".strip("( )").split()
+version = commitInfo[commitInfo.index("tag:") + 1].rstrip(",")
+# version = "1.0.0"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="caspeak",
+    version=version,
+    description="Finding non-reference mobile element insertions (MEIs) based on outer-Cas9 targeted Nanopore sequencing and peak detection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rye-lxy/CasPeak",
+    author="Xinyi Liu",
+    install_requires=["pillow"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+    ],
+    scripts=["caspeak"],
+)
